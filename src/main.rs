3ccf8458edf06a0082db92a84fd434f34d7374e0
Juho0 <73646140+Juho0@users.noreply.github.com>
2022-08-22 12:50:09 +0200

    optinal edit-nr flag added

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,8 @@ struct Opts {
     r1_in: Vec<String>,
     #[clap(long)]
     r2_in: Vec<String>,
+    #[clap(long)]
+    edit_nr: bool,
     #[clap(subcommand)]
     sub: Commands,
 }
@@ -138,6 +140,10 @@ fn main() {
     let pb2 = m.insert_after(&pb, ProgressBar::new(len.try_into().unwrap()));
     pb2.set_style(style.clone());
     println!("[1/1] Transfering UMI to records...");
+    let mut edit_nr = false;
+    if args.edit_nr {
+        edit_nr = true;
+    }
     match args.sub {
         Commands::Separate { ru_in } => {
             let ru1 = ru_in.clone();
@@ -166,7 +172,7 @@ fn main() {
                             r2_rec.unwrap(),
                             write_file_r2,
                             ru_rec.unwrap().seq(),
-                            true,
+                            edit_nr,
                         );
                     }
                     pb2.finish_with_message("R2 done");
@@ -203,7 +209,7 @@ fn main() {
                         pb2.set_message("R2");
                         pb2.inc(1);
                         let record2 = extract(r2_rec.unwrap(), &(pattern2.as_ref().unwrap()));
-                        write_file_r2 = write_inline_to_file(record2, write_file_r2, true);
+                        write_file_r2 = write_inline_to_file(record2, write_file_r2, edit_nr);
                     }
                     pb2.finish_with_message("R2 done");
                 });
