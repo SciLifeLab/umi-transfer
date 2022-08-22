dc6e30fdd1e48a7cbaab0bd4391805e399bfefc2
Juho0 <73646140+Juho0@users.noreply.github.com>
2022-08-22 13:03:48 +0200

    Updated progressbar message

---
+++ target/debug/deps/umi_transfer-84f41e07eacb1dd1.d
@@ -0,0 +1,5 @@
+/Users/judit/umi-transfer/target/debug/deps/umi_transfer-84f41e07eacb1dd1: src/main.rs
+
+/Users/judit/umi-transfer/target/debug/deps/umi_transfer-84f41e07eacb1dd1.d: src/main.rs
+
+src/main.rs:
