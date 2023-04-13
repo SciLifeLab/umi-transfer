9a62adc1335379aa8ddd73e8af48a3ac560afcd8
Johannes Alneberg <alneberg@kth.se>
2023-04-13 16:05:47 +0200

    Tidying up a bit

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ enum ReadFile {
     Fastq(File),
     Gzip(Gzip),
 }
+
 impl std::io::Read for ReadFile {
     // Implement read for ReadFile enum
     fn read(&mut self, into: &mut [u8]) -> std::io::Result<usize> {
@@ -21,10 +22,12 @@ impl std::io::Read for ReadFile {
         }
     }
 }
+
 // Enum for the two accepted output formats, '.fastq' and '.fastq.gz'
 enum OutputFile {
     Fastq { read: bio::io::fastq::Writer<File> },
 }
+
 impl OutputFile {
     // Implement write for OutputFile enum
     fn write(self, header: &str, desc: Option<&str>, s: bio::io::fastq::Record) -> OutputFile {
@@ -36,6 +39,7 @@ impl OutputFile {
         }
     }
 }
+
 // Read input file to Reader. Automatically scans if gzipped from .gz suffix
 fn read_fastq(path: &str) -> bio::io::fastq::Reader<std::io::BufReader<ReadFile>> {
     if path.ends_with(".gz") {
@@ -49,6 +53,7 @@ fn read_fastq(path: &str) -> bio::io::fastq::Reader<std::io::BufReader<ReadFile>
         bio::io::fastq::Reader::new(ReadFile::Fastq(std::fs::File::open(path).unwrap()))
     }
 }
+
 // Create output files
 fn output_file(name: &str) -> OutputFile {
     OutputFile::Fastq {
@@ -62,16 +67,22 @@ fn output_file(name: &str) -> OutputFile {
 #[clap(
     version = "0.2.0",
     author = "Judit Hohenthal, Matthias Zepper, Johannes Alneberg",
-    about = "A tool for transfering Unique Molecular Identifiers (UMIs)."
+    about = "A tool for transfering Unique Molecular Identifiers (UMIs). \n\nThe UMIs are given as a fastq file and will be transferred, explaining the name umi-transfer, to the header of the first two fastq files. \n\n"
 )]
 struct Opts {
     #[clap(
         long,
-        default_value = "integrated",
+        default_value = "output",
         help = "Prefix for output files, omitted flag will result in default value.
         \n "
     )]
     prefix: String,
+    #[clap(
+        long,
+        help = "Automatically change '3' into '2' in sequence header of output file from R3.
+        \n "
+    )]
+    edit_nr: bool,
     #[clap(
         long,
         required = true,
@@ -81,17 +92,17 @@ struct Opts {
     r1_in: Vec<String>,
     #[clap(
         long,
-        help = "Input file 2 with reads.
+        required = true,
+        help = "[REQUIRED] Input file 2 with reads.
     \n "
     )]
     r2_in: Vec<String>,
     #[clap(
         long,
-        help = "Automatically change '3' into '2' in header of output file from R3.
-        \n "
+        required = true,
+        help = "[REQUIRED] Input file with UMI.
+        \n"
     )]
-    edit_nr: bool,
-    #[clap(long, required = true)]
     ru_in: Vec<String>,
 }
 
@@ -100,10 +111,10 @@ fn write_to_file(
     input: bio::io::fastq::Record,
     output: OutputFile,
     umi: &[u8],
-    second: bool,
+    edit_nr: bool,
 ) -> OutputFile {
     let s = input;
-    if second {
+    if edit_nr {
         let header = &[s.id(), ":", std::str::from_utf8(&umi).unwrap()].concat();
         let mut string = String::from(s.desc().unwrap());
         string.replace_range(0..1, "2");
@@ -119,32 +130,31 @@ fn main() {
     // Parse commandline arguments
     let args = Opts::parse();
 
-    // Create write files, not gzipped if --no-gzip flag entered.
-    let mut write_file_r1 = output_file(&format!("{}1", &args.prefix));
-
-    // Create a record iterator from input file 1
-    let r1 = read_fastq(&args.r1_in[0]).records();
-
-    println!("[1/1] Transfering UMI to records...");
-
     // Enables editing id in output file 2 if --edit-nr flag was included
     let mut edit_nr = false;
     if args.edit_nr {
         edit_nr = true;
     }
 
-    let ru = read_fastq(&args.ru_in[0]).records();
+    // Create fastq record iterators from input files
+    let r1 = read_fastq(&args.r1_in[0]).records();
     let r2 = read_fastq(&args.r2_in[0]).records();
+    let ru = read_fastq(&args.ru_in[0]).records();
+
+    // Create write files.
+    let mut write_file_r1 = output_file(&format!("{}1", &args.prefix));
     let mut write_file_r2 = output_file(&format!("{}2", &args.prefix));
 
-    // Iterate records in input file and UMI file
-    for (r1_rec, ru_rec, r2_rec) in izip!(r1, ru, r2) {
-        let ru_rec2 = ru_rec.unwrap(); // Error "handling"
-        let ru2 = ru_rec2.clone();
+    println!("Transfering UMIs to records...");
 
-        // Write to Output file
-        write_file_r1 = write_to_file(r1_rec.unwrap(), write_file_r1, ru_rec2.seq(), false);
+    // Iterate over records in input files
+    for (r1_rec, ru_rec_res, r2_rec) in izip!(r1, ru, r2) {
+        let ru_rec = ru_rec_res.unwrap();
+        // Write to Output file (never edit nr for R1)
+        write_file_r1 = write_to_file(r1_rec.unwrap(), write_file_r1, ru_rec.seq(), false);
 
-        write_file_r2 = write_to_file(r2_rec.unwrap(), write_file_r2, ru2.seq(), edit_nr);
+        let ru_rec2 = ru_rec.clone();
+        // Write to Output file (edit nr for R2 if --edit-nr flag was included)
+        write_file_r2 = write_to_file(r2_rec.unwrap(), write_file_r2, ru_rec2.seq(), edit_nr);
     }
 }
