b2c502bc62c1f9f113ab44e5d0988f9c05991fcf
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-05-03 20:32:39 +0200

    Implemented some basic error handling.

--- src/umi_external.rs
+++ src/umi_external.rs
@@ -1,7 +1,8 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use itertools::izip;
 
 use super::file_io;
+use crate::umi_errors::RuntimeErrors;
 
 pub fn run(args: super::Opts) -> Result<()> {
     // Enables editing id in output file 2 if --edit-nr flag was included
@@ -22,25 +23,27 @@ pub fn run(args: super::Opts) -> Result<()> {
     println!("Transferring UMIs to records...");
 
     // Iterate over records in input files
-    for (r1_rec, ru_rec_res, r2_rec) in izip!(r1, ru, r2) {
+    for (r1_rec_res, ru_rec_res, r2_rec_res) in izip!(r1, ru, r2) {
+        let r1_rec = r1_rec_res
+            .with_context(|| format!("Failed to read records from {}", &args.r1_in[0]))?;
+        let r2_rec = r2_rec_res
+            .with_context(|| format!("Failed to read records from {}", &args.r2_in[0]))?;
         let ru_rec = ru_rec_res
             .with_context(|| format!("Failed to read records from {}", &args.ru_in[0]))?;
 
-        // Write to Output file (never edit nr for R1)
-        write_file_r1 = file_io::write_to_file(
-            r1_rec.with_context(|| format!("Failed to read records from {}", &args.r1_in[0]))?,
-            write_file_r1,
-            &ru_rec.seq(),
-            false,
-        );
+        if r1_rec.id().eq(ru_rec.id()) {
+            // Write to Output file (never edit nr for R1)
+            write_file_r1 = file_io::write_to_file(r1_rec, write_file_r1, &ru_rec.seq(), false);
+        } else {
+            return Err(anyhow!(RuntimeErrors::ReadIDMismatchError));
+        }
 
-        // Write to Output file (edit nr for R2 if --edit-nr flag was included)
-        write_file_r2 = file_io::write_to_file(
-            r2_rec.with_context(|| format!("Failed to read records from {}", &args.r2_in[0]))?,
-            write_file_r2,
-            &ru_rec.seq(),
-            edit_nr,
-        );
+        if r2_rec.id().eq(ru_rec.id()) {
+            // Write to Output file (edit nr for R2 if --edit-nr flag was included)
+            write_file_r2 = file_io::write_to_file(r2_rec, write_file_r2, &ru_rec.seq(), edit_nr);
+        } else {
+            return Err(anyhow!(RuntimeErrors::ReadIDMismatchError));
+        }
     }
     Ok(())
 }
