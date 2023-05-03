723b08bef3ad1e2430c939170d08f0678c3c30ff
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-05-03 15:40:22 +0200

    Adding some basic error handling.

--- src/umi_external.rs
+++ src/umi_external.rs
@@ -1,8 +1,9 @@
+use anyhow::{Context, Result};
 use itertools::izip;
 
 use super::file_io;
 
-pub fn run(args: super::Opts) {
+pub fn run(args: super::Opts) -> Result<()> {
     // Enables editing id in output file 2 if --edit-nr flag was included
     let mut edit_nr = false;
     if args.edit_nr {
@@ -18,17 +19,28 @@ pub fn run(args: super::Opts) {
     let mut write_file_r1 = file_io::output_file(&format!("{}1", &args.prefix), args.gzip);
     let mut write_file_r2 = file_io::output_file(&format!("{}2", &args.prefix), args.gzip);
 
-    println!("Transfering UMIs to records...");
+    println!("Transferring UMIs to records...");
 
     // Iterate over records in input files
     for (r1_rec, ru_rec_res, r2_rec) in izip!(r1, ru, r2) {
-        let ru_rec = ru_rec_res.unwrap();
-        // Write to Output file (never edit nr for R1)
-        write_file_r1 = file_io::write_to_file(r1_rec.unwrap(), write_file_r1, ru_rec.seq(), false);
+        let ru_rec = ru_rec_res
+            .with_context(|| format!("Failed to read records from {}", &args.ru_in[0]))?;
 
-        let ru_rec2 = ru_rec.clone();
+        // Write to Output file (never edit nr for R1)
+        write_file_r1 = file_io::write_to_file(
+            r1_rec.with_context(|| format!("Failed to read records from {}", &args.r1_in[0]))?,
+            write_file_r1,
+            &ru_rec.seq(),
+            false,
+        );
+
         // Write to Output file (edit nr for R2 if --edit-nr flag was included)
-        write_file_r2 =
-            file_io::write_to_file(r2_rec.unwrap(), write_file_r2, ru_rec2.seq(), edit_nr);
+        write_file_r2 = file_io::write_to_file(
+            r2_rec.with_context(|| format!("Failed to read records from {}", &args.r2_in[0]))?,
+            write_file_r2,
+            &ru_rec.seq(),
+            edit_nr,
+        );
     }
+    Ok(())
 }
