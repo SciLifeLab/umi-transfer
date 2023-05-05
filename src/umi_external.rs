c62c17a9b9ec4e54c1982b336d53c0640332ded8
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-05-05 20:54:01 +0200

    Implemented a simple counter for the records.

--- src/umi_external.rs
+++ src/umi_external.rs
@@ -21,35 +21,39 @@ pub struct OptsExternal {
     )]
     edit_nr: bool,
     #[clap(
-        long,
+        short = '1',
+        long = "in1",
         required = true,
         help = "[REQUIRED] Input file 1 with reads.
     \n "
     )]
-    r1_in: Vec<String>,
+    r1_in: PathBuf,
     #[clap(
-        long,
+        short = '2',
+        long = "in2",
         required = true,
         help = "[REQUIRED] Input file 2 with reads.
     \n "
     )]
-    r2_in: Vec<String>,
+    r2_in: PathBuf,
     #[clap(
-        long,
+        short = 'u',
+        long = "umi",
         required = true,
         help = "[REQUIRED] Input file with UMI.
         \n"
     )]
-    ru_in: Vec<String>,
+    ru_in: PathBuf,
     #[clap(
-        long,
+        short = 'z',
+        long = "gzip",
         help = "Compress output files with gzip. By default turned off to encourage use of external compression (see Readme).
         \n "
     )]
     gzip: bool,
 }
 
-pub fn run(args: OptsExternal) -> Result<()> {
+pub fn run(args: OptsExternal) -> Result<i32> {
     // Enables editing id in output file 2 if --edit-nr flag was included
     let mut edit_nr = false;
     if args.edit_nr {
@@ -57,24 +61,42 @@ pub fn run(args: OptsExternal) -> Result<()> {
     }
 
     // Create fastq record iterators from input files
-    let r1 = file_io::read_fastq(&args.r1_in[0]).records();
-    let r2 = file_io::read_fastq(&args.r2_in[0]).records();
-    let ru = file_io::read_fastq(&args.ru_in[0]).records();
+    let r1 = file_io::read_fastq(&args.r1_in).records();
+    let r2 = file_io::read_fastq(&args.r2_in).records();
+    let ru = file_io::read_fastq(&args.ru_in).records();
 
     // Create write files.
     let mut write_file_r1 = file_io::output_file(&format!("{}1", &args.prefix), args.gzip);
     let mut write_file_r2 = file_io::output_file(&format!("{}2", &args.prefix), args.gzip);
 
+    // Record counter
+    let mut counter: i32 = 0;
+
     println!("Transferring UMIs to records...");
 
     // Iterate over records in input files
     for (r1_rec_res, ru_rec_res, r2_rec_res) in izip!(r1, ru, r2) {
-        let r1_rec = r1_rec_res
-            .with_context(|| format!("Failed to read records from {}", &args.r1_in[0]))?;
-        let r2_rec = r2_rec_res
-            .with_context(|| format!("Failed to read records from {}", &args.r2_in[0]))?;
-        let ru_rec = ru_rec_res
-            .with_context(|| format!("Failed to read records from {}", &args.ru_in[0]))?;
+        let r1_rec = r1_rec_res.with_context(|| {
+            format!(
+                "Failed to read records from {}",
+                &args.r1_in.to_string_lossy()
+            )
+        })?;
+        let r2_rec = r2_rec_res.with_context(|| {
+            format!(
+                "Failed to read records from {}",
+                &args.r2_in.to_string_lossy()
+            )
+        })?;
+        let ru_rec = ru_rec_res.with_context(|| {
+            format!(
+                "Failed to read records from {}",
+                &args.ru_in.to_string_lossy()
+            )
+        })?;
+
+        // Step counter
+        counter += 1;
 
         if r1_rec.id().eq(ru_rec.id()) {
             // Write to Output file (never edit nr for R1)
@@ -90,5 +112,6 @@ pub fn run(args: OptsExternal) -> Result<()> {
             return Err(anyhow!(RuntimeErrors::ReadIDMismatchError));
         }
     }
-    Ok(())
+    println!("Processed {:?} records", counter);
+    Ok(counter)
 }
