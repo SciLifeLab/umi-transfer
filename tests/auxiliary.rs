c3db2464186ed236093d0cd8b3f32523e3306e7d
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-07-12 21:03:11 +0200

    Added tests for gz file extension.

--- tests/auxiliary.rs
+++ tests/auxiliary.rs
@@ -4,7 +4,10 @@ use assert_fs::prelude::*;
 use std::path::PathBuf;
 use std::process::Command;
 
+// since those are just needed for the tests, I didn't put it in src. Therefore, using this module is not detected and dead_code warnings issued.
+
 #[derive()]
+#[allow(dead_code)]
 pub struct TestFiles {
     // Struct to hold the paths to test files.
     pub read1: PathBuf,
@@ -16,8 +19,13 @@ pub struct TestFiles {
     pub umi_shuffle: PathBuf,
     pub umi_shuffle_gz: PathBuf,
     pub existing_output: NamedTempFile,
+    pub new_output_read1: PathBuf,
+    pub new_output_read1_gz: PathBuf,
+    pub new_output_read2: PathBuf,
+    pub new_output_read2_gz: PathBuf,
 }
 
+#[allow(dead_code)]
 pub fn setup_integration_test() -> (Command, TempDir, TestFiles) {
     // Get the name of the binary (umi-transfer)
     let cmd = Command::cargo_bin(assert_cmd::crate_name!())
@@ -41,9 +49,13 @@ pub fn setup_integration_test() -> (Command, TempDir, TestFiles) {
         read2_gz: temp_dir.path().join("read2.fq.gz"),
         umi: temp_dir.path().join("umi.fq"),
         umi_gz: temp_dir.path().join("umi.fq.gz"),
-        umi_shuffle: temp_dir.path().join("umi_shuffle.fq"),
-        umi_shuffle_gz: temp_dir.path().join("umi_shuffle.fq.gz"),
+        umi_shuffle: temp_dir.path().join("umi_shuffled.fq"),
+        umi_shuffle_gz: temp_dir.path().join("umi_shuffled.fq.gz"),
         existing_output: NamedTempFile::new("ACTG.fq").unwrap(),
+        new_output_read1: temp_dir.path().join("read1_out.fq"),
+        new_output_read1_gz: temp_dir.path().join("read1_out.fq.gz"),
+        new_output_read2: temp_dir.path().join("read2_out.fq"),
+        new_output_read2_gz: temp_dir.path().join("read2_out.fq.gz"),
     };
 
     return (cmd, temp_dir, test_files);
--- tests/integration_tests_external.rs
+++ tests/integration_tests_external.rs
@@ -1,4 +1,5 @@
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::*;
 use std::process::Command;
 
@@ -38,6 +39,14 @@ fn external_with_minimal_arguments_plain() {
         .stdout(predicate::str::contains("Processed 10 records"))
         .stdout(predicate::str::contains("umi-transfer finished after"));
 
+    temp_dir
+        .child("read1_with_UMIs.fq")
+        .assert(predicate::path::exists());
+
+    temp_dir
+        .child("read2_with_UMIs.fq")
+        .assert(predicate::path::exists());
+
     temp_dir.close().unwrap();
 }
 
@@ -50,13 +59,90 @@ fn external_with_minimal_arguments_compressed() {
         .arg("--in2")
         .arg(test_files.read2_gz)
         .arg("--umi")
-        .arg(test_files.umi_gz);
+        .arg(test_files.umi_gz)
+        .arg("--gzip");
 
     cmd.assert()
         .success()
         .stdout(predicate::str::contains("Transferring UMIs to records"))
         .stdout(predicate::str::contains("Processed 10 records"))
         .stdout(predicate::str::contains("umi-transfer finished after"));
+
+    temp_dir
+        .child("read1_with_UMIs.fq.gz")
+        .assert(predicate::path::exists());
+
+    temp_dir
+        .child("read2_with_UMIs.fq.gz")
+        .assert(predicate::path::exists());
+
+    temp_dir.close().unwrap();
+}
+
+#[test]
+fn external_with_output_gz_suffix_no_compression() {
+    let (mut cmd, temp_dir, test_files) = auxiliary::setup_integration_test();
+    cmd.arg("external")
+        .arg("--in")
+        .arg(test_files.read1_gz)
+        .arg("--in2")
+        .arg(test_files.read2_gz)
+        .arg("--umi")
+        .arg(test_files.umi_gz)
+        .arg("--out")
+        .arg(test_files.new_output_read1_gz)
+        .arg("--out2")
+        .arg(test_files.new_output_read2_gz);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Transferring UMIs to records"))
+        .stdout(predicate::str::contains("Processed 10 records"))
+        .stdout(predicate::str::contains("umi-transfer finished after"));
+
+    // Even though --out "read1_out.fq.gz" and --out2 "read2_out.fq.gz" are explicitly specified,
+    // the .gz suffix will be automatically removed if no -z / --gzip was chosen.
+    // This behavior was chosen because of FIFOs use.
+
+    temp_dir
+        .child("read1_out.fq")
+        .assert(predicate::path::exists());
+
+    temp_dir
+        .child("read2_out.fq")
+        .assert(predicate::path::exists());
+
+    temp_dir
+        .child("read1_out.fq.gz")
+        .assert(predicate::path::missing());
+
+    temp_dir
+        .child("read2_out.fq.gz")
+        .assert(predicate::path::missing());
+
+    temp_dir.close().unwrap();
+}
+
+#[test]
+fn external_fails_on_read_id_mismatch() {
+    let (mut cmd, temp_dir, test_files) = auxiliary::setup_integration_test();
+    cmd.arg("external")
+        .arg("--in")
+        .arg(test_files.read1_gz)
+        .arg("--in2")
+        .arg(test_files.read2_gz)
+        .arg("--umi")
+        .arg(test_files.umi_shuffle_gz);
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("Failed to include the UMIs"))
+        .stderr(predicate::str::contains(
+            "IDs of UMI and read records mismatch",
+        ))
+        .stderr(predicate::str::contains(
+            "Please provide sorted files as input",
+        ));
 
     temp_dir.close().unwrap();
 }
