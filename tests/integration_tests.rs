059354c2035e8d8bdddb9d330cff69183ba354e4
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-07-12 20:01:37 +0200

    Add two more integration tests.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -62,7 +62,23 @@ fn main_without_arguments_prints_help() {
 }
 
 #[test]
-fn external_with_minimal_arguments() {
+fn external_fails_without_arguments() {
+    let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+
+    cmd.arg("external");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "error: the following required arguments were not provided",
+        ))
+        .stderr(predicate::str::contains("--in <R1_IN>"))
+        .stderr(predicate::str::contains("--in2 <R2_IN>"))
+        .stderr(predicate::str::contains("--umi <RU_IN>"));
+}
+
+#[test]
+fn external_with_minimal_arguments_plain() {
     let (mut cmd, temp_dir, test_files) = setup_integration_test();
     cmd.arg("external")
         .arg("--in")
@@ -71,6 +87,26 @@ fn external_with_minimal_arguments() {
         .arg(test_files.read2)
         .arg("--umi")
         .arg(test_files.umi);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("Transferring UMIs to records"))
+        .stdout(predicate::str::contains("Processed 10 records"))
+        .stdout(predicate::str::contains("umi-transfer finished after"));
+
+    temp_dir.close().unwrap();
+}
+
+#[test]
+fn external_with_minimal_arguments_compressed() {
+    let (mut cmd, temp_dir, test_files) = setup_integration_test();
+    cmd.arg("external")
+        .arg("--in")
+        .arg(test_files.read1_gz)
+        .arg("--in2")
+        .arg(test_files.read2_gz)
+        .arg("--umi")
+        .arg(test_files.umi_gz);
 
     cmd.assert()
         .success()
