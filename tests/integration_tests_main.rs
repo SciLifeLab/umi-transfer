0b9d786696345280d51bbf12eb5790340fddccb8
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2023-07-13 13:58:11 +0200

    Create validated output files to be used in tests and correct header glitch in input.

--- tests/integration_tests_main.rs
+++ tests/integration_tests_main.rs
@@ -5,13 +5,28 @@ use std::process::Command;
 #[path = "auxiliary.rs"]
 mod auxiliary;
 
-// Define the integration tests
 #[test]
 fn main_without_arguments_prints_help() {
     let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+    // Clap prints help to stderr in this case, but to stdout with -h or --help.
     cmd.assert()
         .success()
         .stderr(predicate::str::contains("Usage:"))
         .stderr(predicate::str::contains("Commands:"))
         .stderr(predicate::str::contains("Options:"));
+}
+
+#[test]
+fn main_help_prints_help() {
+    let mut cmd = Command::cargo_bin(assert_cmd::crate_name!()).unwrap();
+
+    // Clap prints help to stdout with -h or --help.
+    for help in &["-h", "--help"] {
+        cmd.arg(help)
+            .assert()
+            .success()
+            .stdout(predicate::str::contains("Usage:"))
+            .stdout(predicate::str::contains("Commands:"))
+            .stdout(predicate::str::contains("Options:"));
+    }
 }
