8d00d87c4ef4acdb3a23837cd11fc5226bbf424d
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2024-05-22 20:07:14 +0200

    Test for compressed output files as well.

--- tests/integration_tests_external_filecontents.rs
+++ tests/integration_tests_external_filecontents.rs
@@ -213,3 +213,41 @@ fn external_switch_umi_and_read2() -> TestResult {
     temp_dir.close()?;
     Ok(())
 }
+
+#[test]
+fn external_produces_correct_compressed_output() -> TestResult {
+    let (mut cmd, temp_dir, test_files, test_output) = auxiliary::setup_integration_test(true);
+    cmd.arg("external")
+        .arg("--in")
+        .arg(test_files.read1)
+        .arg("--in2")
+        .arg(test_files.read2)
+        .arg("--umi")
+        .arg(test_files.umi)
+        .arg("--gzip");
+
+    cmd.assert().success(); //further assertions have been tested in other tests
+
+    temp_dir
+        .child("read1_with_UMIs.fq.gz")
+        .assert(predicate::path::exists());
+
+    temp_dir
+        .child("read2_with_UMIs.fq.gz")
+        .assert(predicate::path::exists());
+
+    let reference = test_output.unwrap();
+
+    verify_file_binary(
+        &temp_dir.child("read1_with_UMIs.fq.gz").to_path_buf(),
+        &reference.compressed_correct_read1,
+    )?;
+
+    verify_file_binary(
+        &temp_dir.child("read2_with_UMIs.fq.gz").to_path_buf(),
+        &reference.compressed_correct_read2,
+    )?;
+
+    temp_dir.close()?;
+    Ok(())
+}
