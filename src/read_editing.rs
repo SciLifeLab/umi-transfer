3f777992d6f8fc0961d46f497b36c7afad6721dd
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2024-12-10 18:39:47 +0100

    Add unit tests for new read_editing.rs file.

--- src/read_editing.rs
+++ src/read_editing.rs
@@ -48,7 +48,9 @@ pub fn umi_to_record_seq(
         let mut new_desc = String::from(input.desc().unwrap());
         new_desc.replace_range(0..1, &number.to_string());
         let desc: Option<&str> = Some(&new_desc);
-        // ToDo: unnecessary conversion to bytes and back to String, but Record::new() does not take arguments
+        // Unnecessary conversion to bytes and back to String, but Record::new() does
+        // not take arguments and the fields of struct `bio::io::fastq::Record` are private,
+        // so I can't implement another method to create a new record.
         let new_record = bio::io::fastq::Record::with_attrs(
             input.id(),
             desc,
@@ -64,5 +66,90 @@ pub fn umi_to_record_seq(
             concatenated_qual_str.as_bytes(),
         );
         Ok(new_record)
+    }
+}
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_umi_to_record_header_with_edits() {
+        let input = bio::io::fastq::Record::with_attrs(
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031",
+            Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"),
+            b"TCGTTTTCCGC",
+            b"FFFFFFFFFFF",
+        );
+        let umi = b"ACCAGCTA";
+        let umi_sep = "_".to_string();
+        let edit_nr = Some(5);
+
+        let result = umi_to_record_header(input, umi, Some(&umi_sep), edit_nr).unwrap();
+        assert_eq!(
+            result.id(),
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031_ACCAGCTA"
+        );
+        assert_eq!(result.desc(), Some("5:N:0:GCTTCAGGGT+AAGGTAGCGT"));
+        assert_eq!(result.seq(), b"TCGTTTTCCGC");
+        assert_eq!(result.qual(), b"FFFFFFFFFFF");
+    }
+
+    #[test]
+    fn test_umi_to_record_header_plain() {
+        let input = bio::io::fastq::Record::with_attrs(
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031",
+            Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"),
+            b"TCGTTTTCCGC",
+            b"FFFFFFFFFFF",
+        );
+        let umi = b"ACCAGCTA";
+        let umi_sep = ":".to_string();
+
+        let result = umi_to_record_header(input, umi, Some(&umi_sep), None).unwrap();
+        assert_eq!(
+            result.id(),
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031:ACCAGCTA"
+        );
+        assert_eq!(result.desc(), Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"));
+        assert_eq!(result.seq(), b"TCGTTTTCCGC");
+        assert_eq!(result.qual(), b"FFFFFFFFFFF");
+    }
+
+    #[test]
+    fn test_umi_to_record_seq_with_edit_nr() {
+        let input = bio::io::fastq::Record::with_attrs(
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031",
+            Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"),
+            b"TCGTTTTCCGC",
+            b"!!!!",
+        );
+        let umi = b"ACCAGCTA";
+        let umi_qual = b"########";
+        let edit_nr = Some(5);
+
+        let result = umi_to_record_seq(input, umi, umi_qual, edit_nr).unwrap();
+        assert_eq!(result.id(), "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031");
+        assert_eq!(result.desc(), Some("5:N:0:GCTTCAGGGT+AAGGTAGCGT"));
+        assert_eq!(result.seq(), b"ACCAGCTATCGTTTTCCGC");
+        assert_eq!(result.qual(), b"########FFFFFFFFFFF");
+    }
+
+    #[test]
+    fn test_umi_to_record_seq_without_edit_nr() {
+        let input = bio::io::fastq::Record::with_attrs(
+            "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031",
+            Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"),
+            b"TCGTTTTCCGC",
+            b"FFFFFFFFFFF",
+        );
+        let umi = b"ACCAGCTA";
+        let umi_qual = b"########";
+
+        let result = umi_to_record_seq(input, umi, umi_qual, None).unwrap();
+        assert_eq!(result.id(), "@SCILIFELAB:500:NGISTLM:1:1101:2446:1031");
+        assert_eq!(result.desc(), Some("1:N:0:GCTTCAGGGT+AAGGTAGCGT"));
+        assert_eq!(result.seq(), b"ACCAGCTATCGTTTTCCGC");
+        assert_eq!(result.qual(), b"########FFFFFFFFFFF");
     }
 }
